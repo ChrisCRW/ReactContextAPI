8f9b86821150e45c42eacff6871cc78081f4d3fa
ChrisCRW <chriscrw@hotmail.co.uk>
2020-04-03 13:09:41 +0100

    Add files via upload

---
+++ src/components/Player.js
@@ -0,0 +1,39 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Counter from './Counter';
+import { Consumer } from './Context';
+
+class Player extends PureComponent {
+
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    score: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+    index: PropTypes.number.isRequired
+  };
+
+  render() {
+    
+    const { index} = this.props;
+
+    return (
+      <div className="player">
+
+        <Consumer>
+          {({actions, players}) => (
+            <span className="player-name">
+            <button className="remove-player" onClick={() => actions.removePlayer(players[index].id)}>✖</button>
+            { players[index].name }
+            </span>
+          )}
+        </Consumer>
+  
+        <Counter 
+          index={index}
+        />
+      </div>
+    );
+  }
+}
+
+export default Player;
